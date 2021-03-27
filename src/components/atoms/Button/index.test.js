dddfbacb6772e9b1415336f4cae6c0016a90013b
prpr48 <prpr48@naver.com>
2021-03-28 02:16:06 +0900

    add Button test

---
+++ src/components/atoms/Button/index.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import { Default } from './index.stories';
+
+it('renders the button in the default state', () => {
+  render(<Default />);
+  expect(screen.getByRole('button')).toHaveTextContent('Hello');
+});
