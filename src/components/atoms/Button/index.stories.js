516bb953be0459bf65f3da1d362a4423edc0056d
prpr48 <prpr48@naver.com>
2021-03-28 02:16:01 +0900

    add Button story

---
+++ src/components/atoms/Button/index.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Button } from './';
+
+export default {
+  title: 'Design System/Atoms/Button',
+  component: Button,
+};
+
+const Template = (args) => <Button {...args}>Hello</Button>;
+
+export const Default = Template.bind({});
